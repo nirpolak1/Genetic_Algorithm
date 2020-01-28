4c1980533267b2840624d36bf26f71d5b7552deb
nirpolak1 <nirpolak1@gmail.com>
2020-01-28 20:47:29 +0200

    Add files via upload

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -6,19 +6,36 @@ Created on Sat Jan 11 20:32:03 2020
 """
 
 import numpy as np
+import cv2
 
-def cost_function(x,y):
-    result = ((y*np.sin(x+y))+(x*np.cos(y)))/(20+np.power(x,2))
+
+###Problem 2
+def cost_function_image(parameters):
+    image = cv2.imread('64652.jpg',0).flatten()
+
+    result = np.sum(abs(parameters - image))
+
     return result
 
 def fitness_calculator(population):
     # calulates the result of a linear problem (for now...)
-    #fitness_values = np.sum(parameters*cost_function, axis=1)
+    fitness_values = np.zeros(population.shape[0])
+    for i in range (population.shape[0]):
+        fitness_values[i] = 1/(cost_function_image(population[i,:]))
+    return fitness_values
+
+###Problem 1
+def cost_function_1(x,y):
+    result = ((y*np.sin(x+y))+(x*np.cos(y)))/(20+np.power(x,2))
+    return result
+
+def fitness_calculator_1(population):
+    # calulates the result of a linear problem (for now...)
     fitness_values = np.zeros(population.shape[0])
     for i in range (population.shape[0]):
         x=population[i,0]
         y=population[i,1]
-        fitness_values[i] = cost_function(x,y)
+        fitness_values[i] = cost_function_1(x,y)
     return fitness_values
 
 def select_parents(population, fitness_values, number_of_parents):
@@ -32,6 +49,28 @@ def select_parents(population, fitness_values, number_of_parents):
         #erasing the best current fitness to move to the next best fitness:
         fitness_values[max_fitness_index] = -999999999
     return parents
+
+
+def sus_selection(population, fitness_values,number_of_parents):
+    #creating an empty array to hold the indexes of selected parents among the population
+    parents_index = np.empty(number_of_parents)
+    #making sure all fitness values for roulette sections are positive
+    fitness_values = fitness_values - np.min(fitness_values)
+    sum_of_fitness = np.sum(fitness_values)
+    pointer_step = 1/number_of_parents
+    pointer = np.random.uniform(0.0,1.0)%pointer_step
+    roulette_i = 0.0
+    selection_index = 0
+    #selecting the individuals where a roulette pointer lies and recording their index:
+    for i in range(0,fitness_values.shape[0]):
+        roulette_f = roulette_i + fitness_values[i]/sum_of_fitness
+        if (roulette_i <= pointer < roulette_f):
+            parents_index[selection_index] = np.uint8(i)
+            selection_index += 1
+            pointer += pointer_step
+        roulette_i = roulette_f
+    
+    return parents_index
 
 def crossover_mating(parents, number_of_offsprings):
     # completeing the population size with new sets
@@ -52,6 +91,18 @@ def crossover_mating(parents, number_of_offsprings):
         offsprings[index, crossover_at:] = parents[second_parent, crossover_at:]
     return offsprings
 
+def sus_crossover(parents_index, population, number_of_offsprings):
+    offsprings = np.empty(number_of_offsprings)
+    for index in range(number_of_offsprings[0]):
+        #selecting the parents by order of fitness:
+        first_parent = np.uint8(parents_index[index])
+        second_parent = np.uint8(parents_index[(index+1)%parents_index.shape[0]])
+        #randomly choosing which parameters would be taken from the first or second parent:
+        crossover_list = np.random.randint(0,2,number_of_offsprings[1])
+        #creating the offspring:
+        offsprings[index,:] = population[first_parent,:]*crossover_list + population[second_parent,:]*(1-crossover_list)
+    return offsprings
+
 def mutate(offsprings,parameters_range):
     #mutating a random parameter in each offspring:
     for index in range(offsprings.shape[0]):
@@ -62,4 +113,29 @@ def mutate(offsprings,parameters_range):
         offsprings[index,random_gene] = offsprings[index,random_gene] + mutation_size
     return offsprings
 
+def mutation_rating(fitness_values,base_rate):
+    top_range = np.max(fitness_values) - np.mean(fitness_values)
+    bottom_range = np.mean(fitness_values) - np.min(fitness_values)
+    mutation_rate = base_rate*np.amax([np.amin([top_range/bottom_range,10.0]),0.1])
+    return mutation_rate
+    
+def sus_mutate(offsprings,parameters_range,rate):
+    #mutating a random parameter in each offspring:
+    for index in range(offsprings.shape[0]):
+        mutation_vector = np.random.uniform(0.0,1.0,offsprings.shape[1])
+        mutation_vector = np.where(mutation_vector <= rate, 1 ,0)
+        offsprings[index,:] = mutation_vector*np.random.uniform(parameters_range[0,0],parameters_range[1,0],offsprings.shape[1]) + (1-mutation_vector)*offsprings[index,:]
+    return offsprings
 
+def sus_insertion(offsprings, population, number_of_parents, fitness_values):
+    parents = np.empty((number_of_parents, population.shape[1]))
+    #selecting the parents by decreasing fitness values:
+    for parent_index in range(number_of_parents):
+        max_fitness_index = np.where(fitness_values == np.max(fitness_values))
+        max_fitness_index = max_fitness_index[0][0]
+        parents[parent_index, :] = population[max_fitness_index,:]
+        #erasing the best current fitness to move to the next best fitness:
+        fitness_values[max_fitness_index] = -999999999
+    population[0:number_of_parents,:] = parents
+    population[number_of_parents:,:] = offsprings
+    return population
--- problem_1.py
+++ problem_1.py
@@ -9,33 +9,25 @@ import numpy as np
 import geneticalgorithm as ga
 
 #defining a cost function:
-number_of_parameters = 2
+number_of_parameters = 12604
+
+#defining mutation base rate:
+base_rate=0.005
 
 #defining the size of the population:
-number_of_individuals = 5
+number_of_individuals = 100
 number_of_parents = int(number_of_individuals/2)
 population_size = (number_of_individuals, number_of_parameters)
 
 #defining the number of generations:
-generations = 20
-
+generations = 1500
 #defining range:
-lower_bound = -5.0
-upper_bound = 5.0
+lower_bound = 0
+upper_bound = 255
 parameters_range = np.zeros((2,number_of_parameters))
 parameters_range[:][0] = lower_bound
 parameters_range[:][1] = upper_bound
 
-#plotting the function space:
-x = np.outer(np.linspace(lower_bound,upper_bound,50),np.ones(50))
-y = x.copy().T
-z = ga.cost_function(x,y)
-
-a_fig = plt.figure(figsize=(15,10))
-search_space = a_fig.add_subplot(1,1,1)
-search_space = plt.axes(projection='3d')
-search_space.plot_surface(x,y,z,cmap='afmhot',edgecolor='none', alpha =0.5)
-search_space.set_title('Cost Function Search Space')
 
 b_fig = plt.figure(figsize=(15,10))
 progress_max = np.zeros(generations)
@@ -44,13 +36,9 @@ progress_min = np.zeros(generations)
 progress_generations = np.linspace(0,generations-1,generations)
 progress_fig = b_fig.add_subplot(1,1,1)
 
-#plt.show()
-
 
 #creating the first generation population:
 population = np.random.uniform(lower_bound,upper_bound,population_size)
-#showing first generation in red
-search_space.scatter(population[:,0],population[:,1],ga.fitness_calculator(population),marker='o',alpha = 1, c='r')
 
 
 for generation in range(generations):
@@ -60,17 +48,14 @@ for generation in range(generations):
     progress_max[generation] = np.max(fitness_values)
     progress_mean[generation] = np.mean(fitness_values)
     progress_min[generation] = np.min(fitness_values)
-    #showing each generation in blue, less faded with each generation
-    search_space.scatter(population[:,0],population[:,1],fitness_values,marker='o',alpha = generation/generations, c='b')
     #selecting the parents:
-    parents = ga.select_parents(population, fitness_values, number_of_parents)
+    parents_index = ga.sus_selection(population, fitness_values,number_of_parents)
     #creating offsprings:
-    offsprings = ga.crossover_mating(parents,((number_of_individuals-number_of_parents),(number_of_parameters)))
+    offsprings = ga.sus_crossover(parents_index, population, ((number_of_individuals-number_of_parents),(number_of_parameters)))
     #mutating offsprings:
-    offsprings_mutated = ga.mutate(offsprings,parameters_range)
-    #assembling the new population:
-    population[0:parents.shape[0],:] = parents
-    population[parents.shape[0]:,:] = offsprings_mutated
+    rate = ga.mutation_rating(fitness_values,base_rate)
+    offsprings_mutated = ga.sus_mutate(offsprings,parameters_range,rate)
+    population = ga.sus_insertion(offsprings, population, number_of_parents, fitness_values)
     
     #show the best result:
     print("Best Fitness:", np.max(ga.fitness_calculator(population)))
@@ -78,9 +63,14 @@ for generation in range(generations):
 progress_fig.fill_between(progress_generations,progress_max,progress_min,edgecolor='black',facecolor='grey')
 progress_fig.plot(progress_generations, progress_mean,c='black')
 progress_fig.plot(progress_generations, progress_max,c='r',linewidth=2)
-a_fig.show()
+plt.show()
+
 #show the best set and best result:
 fitness_values = ga.fitness_calculator(population)
 best_fitness_index = np.where(fitness_values == np.max(fitness_values))
 print("Best Solution:", population[best_fitness_index,:])
 print("Best Solution Fitness:", fitness_values[best_fitness_index])
+
+solution_image = np.reshape(np.uint8(population[best_fitness_index,:]),(137,92))
+plt.imshow(solution_image, cmap = 'gray')
+plt.show()
